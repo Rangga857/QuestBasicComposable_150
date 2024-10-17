8fda0fdabd9df3c8255d0c22787e432f831acee5
Rangga857 <azharrangga28@gmail.com>
2024-10-17 21:46:30 +0700

    menambahkan gambar dari dari direktori R.drawable dengan file logoummy dengan mengkosongkan deskripsi konten

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -76,6 +76,10 @@ fun basiclayout(modifier: Modifier = Modifier){
 
             modifier = Modifier.padding(top = 10.dp)
         )
+        Image(painter = painterResource(id = R.drawable.logoumy),
+            contentDescription = null
+            )
+
     }
 
 }
